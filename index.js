9ea77bf2cd58d43166ed2f1951f39ca540fd0cac
Mayy isme <3hung1998@gmail.com>
2023-11-23 22:51:49 +0700

    source day23-11

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-document.addEventListener("DOMContentLoaded", function () {
+// >>>> Đoạn code này dùng Arrow Function -----
+
+document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector(".search-box");
   const input = form.querySelector('input[type="search"]');
   const resultsContainer = document.querySelector(".results");
   const resultsCounter = document.querySelector("header p");
 
-  form.addEventListener("submit", function (event) {
+  form.addEventListener("submit", (event) => {
     event.preventDefault();
     const searchTerm = input.value;
     if (searchTerm) {
@@ -12,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
-  function searchWikipedia(searchTerm) {
+  const searchWikipedia = (searchTerm) => {
     const url = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=500&srsearch=${encodeURIComponent(
       searchTerm
     )}`;
@@ -23,21 +25,20 @@ document.addEventListener("DOMContentLoaded", function () {
         displayResults(data.query.search);
       })
       .catch((error) => alert("Error : " + error));
-  }
+  };
 
-  function displayResults(results) {
+  const displayResults = (results) => {
     resultsContainer.innerHTML = "";
     resultsCounter.textContent = `Results Count : ${results.length}`;
     results.forEach((result) => {
       const resultElement = document.createElement("div");
       resultElement.className = "result";
       resultElement.innerHTML = `
-
-            <h3>${result.title}</h3>
-            <p>${result.snippet}</p>
-            <a href="https://en.wikipedia.org/?curid=${result.pageid}" target="_blank">Read More</a>
-            `;
+          <h3>${result.title}</h3>
+          <p>${result.snippet}</p>
+          <a href="https://en.wikipedia.org/?curid=${result.pageid}" target="_blank">Read More</a>
+        `;
       resultsContainer.appendChild(resultElement);
     });
-  }
+  };
 });
